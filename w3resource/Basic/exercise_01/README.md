fd21a307b84ac178e78f784372aa0350fdb5f4ee
Ian Chen <chenian317@gmail.com>
2021-09-15 23:03:34 -0300

    Exercise 01

---
+++ w3resource/Basic/exercise_01/README.md
@@ -0,0 +1,1 @@
+Write a program in C++ to print a welcome text in a separate line.
---
+++ w3resource/Basic/exercise_01/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main()
+{
+	//Print the line
+	cout << "Welcome, this is my first C++ Program !" << endl;;
+	cout << "- Ian Chen" << endl;
+
+	return 0;
+}
