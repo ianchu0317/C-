9f7a3d75fa98d07cc4f4f5bd6ba845494be7112f
Ian Chen <chenian317@gmail.com>
2021-09-15 23:08:53 -0300

    Exercise 02

---
+++ w3resource/Basic/exercise_02/README.md
@@ -0,0 +1,5 @@
+Write a program in C++ to print the sum of two numbers. 
+Sample Output:
+Print the sum of two numbers :
+-----------------------------------
+The sum of 29 and 30 is : 59
---
+++ w3resource/Basic/exercise_02/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	//Declare variables
+	int n1 = 29;
+	int n2 = 30;
+
+	cout << "The sum of %d and %30 is : ", n1, n2, n1+n2, << endl;
+
+	return 0;
+}
