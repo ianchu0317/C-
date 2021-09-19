0b8e421c4c6186dfac225e0b0e2d5814a8bca010
Ian Chen <chenian317@gmail.com>
2021-09-18 22:58:56 -0300

    Finished exercise 04

---
+++ w3resource/Basic/exercise_04/solution.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	int n1, n2;
+	std::cout << "- Input variable 1 : ";
+	std::cin >> n1;
+	std::cout << "- Input variable 2 : ";
+	std::cin >> n2;
+
+	std::cout << "The sum of " <<  n1 << " and " << n2 << " is : " << n1 + n2 << std::endl;
+
+	return 0;
+}
