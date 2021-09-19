3a39473003731ceeee94945d2b943d774a11862c
Ian Chen <chenian317@gmail.com>
2021-09-18 23:54:18 -0300

    changes, give up Basic directory

---
+++ w3resource/Basic/exercise_05/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+/*
+void print(string data_type)
+{
+	std::cout << "The maimum limit of " << data_type << " type : " << size << std::endl;
+
+}
+*/
+int main()
+{
+	//print("int data");
+	//int data type
+	std::cout << "The maximum limit of int data type : " << INT_MAX << std::endl;
+	std::cout << "The minimum limit of int data type : " << INT_MIN << std::endl;
+	//Unsigned int data type
+	std::cout << "The maximum limit of int data type : " <<  UINT_MAX << std::endl;
+	std::cout << ""
+	//Long long data type
+	//Unsigned long long data type
+	//
+
+	return 0;
+}
+
